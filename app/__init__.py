a8e53c058c5821726b8e369f35a31a4b58869ba7
Thomas Breydo <tbreydo@gmail.com>
2021-01-11 21:49:31 -0500

    Get rid of instance config file

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,7 @@ from .views.home import home
 from .views.solution_templates import solution_templates
 
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile("config.py")  # instance/config.py
+app = Flask(__name__)
 app.register_blueprint(home)
 app.register_blueprint(solution_templates)
 
