613cb08dec13f500d6c4e4d5a870208d0282ff42
Thomas Breydo <tbreydo@gmail.com>
2021-01-15 12:52:00 -0500

    Implement DEBUG mode with wsgi.py --debug

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,15 @@
+from argparse import ArgumentParser, Namespace
+
 from app import app
 
-if __name__ == "__main__":
+
+def main():
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument("--debug", help="Use Flask's DEBUG server", action="store_true")
+    args: Namespace = parser.parse_args()
+    app.debug = args.debug
     app.run()
+
+
+if __name__ == "__main__":
+    main()
